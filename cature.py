97c965df96115779eb1092d0bb4045be798423c6
Mohamed Elshorbagy <mohamedelshorbagy96@gmail.com>
2018-02-04 13:02:40 +0200

    Load Images and Split to Train and Test

--- cature.py
+++ cature.py
@@ -4,12 +4,15 @@ import time
 cap = cv2.VideoCapture(0)
 count = 0
 limit = 1000 # limit for Number of Saved Images
+time.sleep(10)
+print("Start Capturing")
 while(cap.isOpened()):
     ret, frame = cap.read()
 
     cv2.imshow('frame',frame)
-    cv2.imwrite("images/%d.png" % count , frame)
+    cv2.imwrite("images/class1T/%d.png" % count , frame)
     count += 1
+    print(count)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     if count == limit:
