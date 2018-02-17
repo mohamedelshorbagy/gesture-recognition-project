a2b1414faccb9e893fe1bcf31fcf05cab027efba
Mohamed Elshorbagy <mohamedelshorbagy96@gmail.com>
2018-02-17 18:11:45 +0200

    Cut Polylines

--- skindetection.py
+++ skindetection.py
@@ -10,9 +10,12 @@ minValue = 70
 low_range = np.array([0, 50, 80])
 upper_range = np.array([30, 200, 255])
 
-# low_range = np.array([0, 20, 30])
-# upper_range = np.array([30, 200, 255])
+# low_range = np.array([15, 50, 50])
+# upper_range = np.array([30, 255, 255])
 
+
+# low_range = np.array([94, 100, 100])
+# upper_range = np.array([114, 255, 255])
 
 
 def skinMode(frame):
@@ -42,11 +45,11 @@ def binaryMode(frame):
     ret, res = cv2.threshold(th3, minValue, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     return res
 
-test = cv2.imread("E:\Python\Deep Learning\Tensorflow-Bootcamp-master\Gesture Reco. Project\images2\class1\\192.png")
+# test = cv2.imread("E:\Python\Deep Learning\Tensorflow-Bootcamp-master\Gesture Reco. Project\images2\class1\\192.png")
 
-testSkinMode = skinMode(test)
-cv2.imshow('Test' , testSkinMode)
-cv2.imwrite('images\\Test.png' , testSkinMode)
+# testSkinMode = skinMode(test)
+# cv2.imshow('Test' , testSkinMode)
+# cv2.imwrite('images\\Test.png' , testSkinMode)
 
 
 # kernel = np.ones((15,15),np.uint8)
@@ -57,9 +60,9 @@ while(cap.isOpened()):
     ret, frame = cap.read()
 
     res = skinMode(frame)
-    res2 = binaryMode(frame)
+    # res2 = binaryMode(frame)
     cv2.imshow("res", res)
-    cv2.imshow("res2", res2)
+    # cv2.imshow("res2", res2)
     if count == 0:
         cv2.imwrite("resp.png", res)
     count += 1
--- track.py
+++ track.py
@@ -6,6 +6,9 @@ import pyautogui
 frame = None
 roiPts = []
 inputMode = False
+count = 0
+    #     # img = img[0:int(cap_region_y_end * frame.shape[0]),
+    #     #             int(cap_region_x_begin * frame.shape[1]):frame.shape[1]] 
 
 
 def main():
@@ -14,7 +17,7 @@ def main():
 		help = "path to the (optional) video file")
 	args = vars(ap.parse_args())
 
-	global frame, roiPts, inputMode
+	global frame, roiPts, inputMode,count
 
 
 	if not args.get("video", False):
@@ -28,9 +31,10 @@ def main():
 
 	while True:
 		(grabbed, frame) = camera.read()
-		cv2.rectangle(frame,(384,0),(640,256),(0,0,255),3)
-		
+		if count == 0:
+			cv2.rectangle(frame,(384,0),(640,256),(0,0,255),3)
 		cv2.imshow("frame" , frame)
+		# frame = skinMode(frame)
 		if not grabbed:
 			break
 
@@ -40,7 +44,13 @@ def main():
 
 			(r, roiBox) = cv2.CamShift(backProj, roiBox, termination)
 			pts = np.int0(cv2.boxPoints(r))
-			cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
+			cv2.polylines(frame, [pts], True, (0, 0, 0), 1)
+			mask = np.zeros(frame.shape, dtype=np.uint8)
+			channel_count = frame.shape[2]
+			ignore_mask_color = (255,)*channel_count
+			cv2.fillPoly(mask, [pts], ignore_mask_color)
+			masked_image = cv2.bitwise_and(frame, mask)
+			cv2.imshow('image_masked', masked_image)
 			xPosition = (pts[0][0] + pts[3][0]) / 2  
 			yPosition = (pts[0][1] + pts[1][1]) / 2
 			cv2.circle(frame, (int(xPosition), int(yPosition)), 4, (255, 0, 0), 2)
@@ -59,6 +69,7 @@ def main():
 			cv2.circle(frame, (420,220) , 4, (0, 255, 0), 2)
 			cv2.circle(frame, (520,220) , 4, (0, 255, 0), 2)
 			cv2.imshow("frame", frame)
+			count = count + 1
 			inputMode = True
 			orig = frame.copy()
 
@@ -72,8 +83,9 @@ def main():
 			br = roiPts[np.argmax(s)]
 
 			roi = orig[tl[1]:br[1], tl[0]:br[0]]
-			roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
+			roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+			# average_color = [roi[:, :, i].mean() for i in range(roi.shape[-1])]
 			roiHist = cv2.calcHist([roi], [0], None, [16], [0, 180])
 			roiHist = cv2.normalize(roiHist, roiHist, 0, 255, cv2.NORM_MINMAX)
 			roiBox = (tl[0], tl[1], br[0], br[1])
